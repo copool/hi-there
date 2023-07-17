07a0f603a6b155e4018e69b0bdeb9f807c5edf58
copool <37784764+copool@users.noreply.github.com>
2023-07-17 18:47:43 +0900

    test cummit
    hahahoho

---
+++ HelloWorld_ywkang.java
@@ -0,0 +1,13 @@
+public class HelloWorld_ywkang {
+  public static void main(String[] args) {
+    System.out.println("Hello world! testest");
+
+    int a = 12;
+    int b = 88;
+    int result =0;
+    result = a + b;
+
+    System.out.print(result);
+
+  }
+}
